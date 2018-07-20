0f148701f78525dd4b025ad50b4fffc85e3499f3
Qazzquimby <qazzquimby@yahoo.com>
2018-07-20 14:28:20 -0700

    test

---
+++ UnrealAmplifiedBot/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BotLibrary;
+
+namespace UnrealAmplifiedBot
+{
+    class Program
+    {
+        static CustomGameWrapper game;
+
+        static void Main(string[] args)
+        {
+            game = new CustomGameWrapper();
+
+            game.chat.prefix = ">>";
+            game.AddStartFunc(PrintWelcomeMessage);
+
+
+            game.bots.RequestBot(WrapperBotTeam.Red, 
+                Deltin.CustomGameAutomation.AIHero.Roadhog,
+                Deltin.CustomGameAutomation.Difficulty.Medium,
+                BotRule.SmallerTeam);
+            game.bots.RequestBot(WrapperBotTeam.Red,
+                Deltin.CustomGameAutomation.AIHero.Roadhog,
+                Deltin.CustomGameAutomation.Difficulty.Medium,
+                BotRule.EqualTeams);
+
+            game.bots.RequestBot(WrapperBotTeam.Red,
+                Deltin.CustomGameAutomation.AIHero.McCree,
+                Deltin.CustomGameAutomation.Difficulty.Medium,
+                BotRule.SmallerTeam);
+            game.bots.RequestBot(WrapperBotTeam.Red,
+                Deltin.CustomGameAutomation.AIHero.McCree,
+                Deltin.CustomGameAutomation.Difficulty.Medium,
+                BotRule.EqualTeams);
+
+            game.bots.RequestBot(WrapperBotTeam.Blue,
+                Deltin.CustomGameAutomation.AIHero.Roadhog,
+                Deltin.CustomGameAutomation.Difficulty.Medium,
+                BotRule.SmallerTeam);
+            game.bots.RequestBot(WrapperBotTeam.Blue,
+                Deltin.CustomGameAutomation.AIHero.Roadhog,
+                Deltin.CustomGameAutomation.Difficulty.Medium,
+                BotRule.EqualTeams);
+
+            game.bots.RequestBot(WrapperBotTeam.Blue,
+                Deltin.CustomGameAutomation.AIHero.McCree,
+                Deltin.CustomGameAutomation.Difficulty.Medium,
+                BotRule.SmallerTeam);
+            game.bots.RequestBot(WrapperBotTeam.Blue,
+                Deltin.CustomGameAutomation.AIHero.McCree,
+                Deltin.CustomGameAutomation.Difficulty.Medium,
+                BotRule.EqualTeams);
+
+
+            game.Start();
+
+
+        }
+
+        static void PrintWelcomeMessage()
+        {
+            game.chat.MatchChat("Beep Boop. Starting up.");
+        }
+
+    }
+}
